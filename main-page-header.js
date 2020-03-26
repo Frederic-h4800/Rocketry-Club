bf24a9a8d181a537d9b734dc5ed7eec7058312c2
Frederic <Frederic-h4800@users.noreply.github.com>
2020-03-26 18:07:56 -0400

    Slideshow broken

--- main-page-header.js
+++ main-page-header.js
@@ -4,11 +4,22 @@ showSlides();
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
